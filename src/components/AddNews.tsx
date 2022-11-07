186439f4892636b0a34b23f73c87b39c3b4a7535
E.Sabathier <47697572+emi-sabathier@users.noreply.github.com>
2022-11-07 17:23:35 +0100

    feature: add animation to modal - remove ui, add html - add fadeIn fadeOut modal

--- src/components/AddNews.tsx
+++ src/components/AddNews.tsx
@@ -1,26 +1,91 @@
-import React, { ReactElement, useState } from 'react';
-import { AddNewsForm } from './AddNewsForm';
+import React, { ReactElement, SyntheticEvent, useState } from 'react';
+import { NewsModel } from '../model/NewsModel';
 
 export function AddNews(): ReactElement {
     const [open, setOpen] = useState<boolean>(false);
+    const [inputsValues, setInputValues] = useState<NewsModel>({
+        title: '',
+        text: '',
+        category: '',
+        date: new Date().toISOString().slice(0, 10),
+    });
 
-    const toggleModal = () => {
+    const toggleOpen = (): void => {
         setOpen(!open);
+        setInputValues({
+            title: '',
+            text: '',
+            category: '',
+            date: new Date().toISOString().slice(0, 10),
+        });
+    };
+
+    const handleInputValues = (inputName: string, inputValue: string): void => {
+        setInputValues({
+            ...inputsValues,
+            [inputName]: inputValue,
+        });
+    };
+
+    const handleSubmit = (event: SyntheticEvent): void => {
+        event.preventDefault();
+        console.log('Test requirement --> Log the inputs values:', inputsValues);
     };
 
     return (
-        <header className="header-container">
-            <button className="button-style" onClick={toggleModal}>
-                Ajouter une news
-            </button>
-            {open ? (
-                <>
-                    {/*<div className="modal-bg"></div>*/}
-                    <div className="modal-style">
-                        <AddNewsForm />
+        <>
+            <header className="header-container">
+                <button className="button-style" onClick={() => toggleOpen()}>
+                    Ajouter une news
+                </button>
+            </header>
+            <div className={`modal-style ${open ? 'fadeIn' : 'fadeOut'}`}>
+                <form>
+                    <div className="input-container">
+                        <button type="button" onClick={() => toggleOpen()} className="close-button">
+                            <figure>
+                                <img
+                                    src={`${process.env.PUBLIC_URL}/assets/images/window-close.png`}
+                                    alt="modal close"
+                                    className="close-img"
+                                />
+                            </figure>
+                        </button>
+                        <label htmlFor="title">Titre</label>
+                        <input
+                            value={inputsValues.title}
+                            type="text"
+                            className="input-style"
+                            required={true}
+                            maxLength={50}
+                            placeholder="Titre"
+                            onChange={e => handleInputValues('title', e.target.value)}
+                        />
+                        <label htmlFor="category">Catégorie</label>
+                        <input
+                            value={inputsValues.category}
+                            type="text"
+                            className="input-style"
+                            required={true}
+                            maxLength={50}
+                            placeholder="Catégorie"
+                            onChange={e => handleInputValues('category', e.target.value)}
+                        />
+                        <label htmlFor="description">Contenu</label>
+                        <textarea
+                            value={inputsValues.text}
+                            onChange={e => handleInputValues('text', e.target.value)}
+                            className="textarea-style"
+                            required={true}
+                            placeholder="Description"
+                            rows={8}
+                        />
                     </div>
-                </>
-            ) : null}
-        </header>
+                    <button type="submit" onClick={e => handleSubmit(e)}>
+                        Envoyer
+                    </button>
+                </form>
+            </div>
+        </>
     );
 }
